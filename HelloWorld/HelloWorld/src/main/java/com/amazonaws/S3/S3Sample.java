f0015923e2fa565912e3174d68eec8e3c84f9d45
LipshaRaj <LipshaRaj@Sweety>
2017-07-16 14:46:46 +0530

    Test Commit

--- HelloWorld/HelloWorld/src/main/java/com/amazonaws/S3/S3Sample.java
+++ HelloWorld/HelloWorld/src/main/java/com/amazonaws/S3/S3Sample.java
@@ -41,7 +41,7 @@ import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 /**
- * This sample demonstrates how to make basic requests to Amazon S3 using the
+ *  This sample demonstrates how to make basic requests to Amazon S3 using the
  * AWS SDK for Java.
  * <p>
  * <b>Prerequisites:</b> You must have a valid Amazon Web Services developer
